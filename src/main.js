4bcf1783bd63c7aeb43cca9668a5576d414d9a1a
iZsk <sk.zeeshan1992@gmail.com>
2020-09-12 17:43:51 -0400

    add basic setup with pkg

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/styles/index.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
